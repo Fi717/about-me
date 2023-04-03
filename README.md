320e8e50e184359123f52767559cee49a4207666
Fi Tosun <fdagdas@gmail.com>
2023-04-03 11:24:43 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # about-me
+
+### Class 03 Demo loops
+
+##### Class 4 Links, CSS Positionung, functions
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-let user = prompt("Hi, This is Fi! What's your name?");
-alert("Welcome to my website " + user);
+let userPoints = 0;
 
-let answer1 = prompt("Did you read my bio? yes/no");
+let user = prompt("Hi, This is Fi! What's your name?");
+alert(
+  "Welcome to my website, " +
+    user +
+    ". please answer the questions with yes/no or y/n."
+);
+
+let answer1 = prompt("Did you read my bio?");
 if (answer1 === "yes") {
   console.log("Let's quiz you then!");
   alert("Let's quiz you then!");
@@ -9,17 +15,19 @@ if (answer1 === "yes") {
   alert("You'll have to guess then!");
 }
 
-let answer2 = prompt("What I study architecture or cinema?");
-if (answer2.toLowerCase() === "architecture") {
-  console.log("Correct! You are legend!");
-  alert("Correct! You are legend!");
+let answer2 = prompt("Did I study cinema?");
+if (answer2.toLowerCase() === "yes" || answer2.toLowerCase() === "y") {
+  console.log("No, that is not correct. I studied architecture!");
+  alert("No, that is not correct. I studied architecture!");
 } else {
-  console.log("That is not correct. I studied architecture degree.");
-  alert("That is not correct. I studied architecture degree.");
+  userPoints++;
+  console.log("You are right. I have architecture degree.");
+  alert("You are right. I have architecture degree.");
 }
 
-let answer3 = prompt("Which city do I live now?");
-if (answer3.toLowerCase() == "liverpool") {
+let answer3 = prompt("Have I lived in Liverpool?");
+if (answer3.toLowerCase() === "yes" || answer3.toLowerCase() === "y") {
+  userPoints++;
   console.log("Yes! That is correct!");
   alert("Yes! That is correct!");
 } else {
@@ -27,11 +35,51 @@ if (answer3.toLowerCase() == "liverpool") {
   alert("That is wrong. I live in Liverpool.");
 }
 
-let answer4 = prompt("Last question! What is my favourite sport?");
-if (answer4.toLowerCase() == "tennis") {
-  console.log("Correct. You are amazing!");
-  alert("Correct. You are amazing!");
+let answer4 = prompt("My favourite sport is football, is it correct?");
+if (answer4.toLowerCase() === "yes" || answer4.toLowerCase() === "y") {
+  console.log("No, my favourite sport is tennis!");
+  alert("No, my favourite sport is tennis!");
 } else {
-  console.log("Wrong. It's tennis.");
-  alert("Wrong. It's tennis.");
+  userPoints++;
+  console.log("Well done. My favourite sport is tennis.");
+  alert("Well done. My favourite sport is tennis.");
+}
+
+let answer5 = prompt("Am I right-handed?");
+if (answer5.toLowerCase() === "yes" || answer5.toLowerCase() === "y") {
+  userPoints++;
+  console.log("Yes, I am right-handed!");
+  alert("Yes, I am right-handed!");
+} else {
+  console.log("No points this time. I am right-handed");
+  alert("No points this time. I am right-handed.");
+}
+
+function q7() {
+  const number = 7;
+  let guess = prompt(
+    "What is my lucky number between 1-10? You have 3 attempts to guess right!"
+  );
+
+  for (let i = 3; i >= 0; i--) {
+    if (i === 0) {
+      alert("Unlucky. It is 6.");
+      break;
+    }
+    if (guess < number) {
+      alert("Too low, guess again!");
+      guess = prompt(
+        `Wrong. You now have  ${i} more attempts to get it right!`
+      );
+    } else if (guess > number) {
+      alert("Too high, guess again!");
+      guess = prompt(
+        `Wrong. You now have  ${i} more attempts to get it right!`
+      );
+    } else if (guess === number) {
+      userPoints++;
+      alert("Congratulations, you have" + userPoints + "points!");
+      break;
+    }
+  }
 }
